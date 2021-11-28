062fde9564abc401b7e80e21d9120dba928b4733
Cyber2f08 <cyber.2f08@gmail.com>
2021-11-29 01:45:28 +0700

    empty, gonna add some later

---
+++ src/res/src/game.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <dos.h>
+#include <ctype.h>
+#include <string.h>
+#include <conio.h>
+#include <stdlib.h>
+
+xp[20].yp[20];
+
+void bar(int z) {
+    window(z+1,22,70,25);
+    textcolor(10);
+    
+}
