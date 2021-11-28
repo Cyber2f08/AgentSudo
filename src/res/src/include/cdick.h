2a343cd75c0b6164fb55ef561992d26e66085fac
Cyber2f08 <cyber.2f08@gmail.com>
2021-11-29 01:43:43 +0700

    Headers for botnet

---
+++ src/res/src/include/cdick.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int printf(const char* msg) {
+    cout << msg << endl; 
+}
+
+int puts(const char* msg) {
+    cerr << msg << endl;
+}
---
+++ src/res/src/include/includes.h
@@ -0,0 +1,6 @@
+#include <ws2tcpip.h>
+#include <string>
+#include <io>
+#include <iostream>
+
+#pragma comment(lib, "ws2_32.lib")
